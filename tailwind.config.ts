30eaf1deac1fea9fd72aa504dbb4e83a99033d7f
esflowers <estrada.flsw@gmail.com>
2025-02-14 20:07:46 -0600

    add fonts and primary palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,25 @@ export default {
   theme: {
     extend: {
       colors: {
-        
+        'primary': {
+					'50': '#fafafa',
+					'100': '#f3f4f4',
+					'200': '#e3e4e4',
+					'300': '#d3d4d4',
+					'400': '#a3a4a4',
+					'500': '#737474',
+					'600': '#535454',
+					'700': '#3a3a3a',
+					'800': '#232323',
+					'900': '#131414',
+					'950': '#030404',
+				},
       },
+      fontFamily: {
+        brand: ["var(--brand)", 'system-ui', 'sans-serif'],
+        sans: ["var(--sans)", 'system-ui', 'sans-serif'],
+        mono: ["var(--mono)", 'Courier', 'monospace'],
+      }
     },
   },
   plugins: [],
