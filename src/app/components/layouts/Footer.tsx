d28f99921ff29843efba935b4dcf1a5bbb4bf157
esflowers <estrada.flsw@gmail.com>
2025-02-15 21:17:55 -0600

    add responsive styles to footer

--- src/app/components/layouts/Footer.tsx
+++ src/app/components/layouts/Footer.tsx
@@ -2,13 +2,13 @@ import { IconPV } from "../icons"
 
 export default function Footer() {
     return (
-        <footer className="flex items-center justify-between gap-2 text-primary-500">
+        <footer className="w-full h-fit mt-8 mb-0 bottom-0 flex flex-wrap items-center justify-between gap-2 text-primary-500">
             <IconPV size="16" />
             
             <p className="font-brand text-sm">Profesionalización Virtual</p>
 
             <time dateTime="2025" className="text-xs ml-auto">
-                2024 - 2025 & future
+                <span className="hidden md:inline">2024 - </span>2025<span className="hidden md:inline"> & futuro</span>
             </time>
         </footer>
     )
