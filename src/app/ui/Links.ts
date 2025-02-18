44ad91d7bb404bf3b5b60dde293edc089e146ede
esflowers <estrada.flsw@gmail.com>
2025-02-17 21:32:26 -0600

    add info consts about the company

--- src/app/ui/Links.ts
+++ src/app/ui/Links.ts
@@ -1,6 +1,10 @@
+export const companyMail = 'hectorjavier12@hotmail.com'
+export const companyPhone = '+529613006743'
+export const companyUbication = 'Cuauhtémoc 188, San Jacinto, 29160 Chiapa de Corzo, Chiapas, México'
+
 export const linkCompany = 'https://profesionalizacionvirtual.com/'
-export const linkMail = 'mailto:hectorjavier12@hotmail.com'
-export const linkPhone = 'tel:+529613006743'
+export const linkMail = `mailto:${companyMail}`
+export const linkPhone = `tel:${companyPhone}`
 export const linkMap = 'https://maps.app.goo.gl/HFPZE5p23zdrWXzPA'
 export const linkInterview = 'https://www.facebook.com/share/v/15wJHf8ekQ/'
 
