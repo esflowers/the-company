199822d0cc4d61f656ea17ead5e24f5c8612547c
esflowers <estrada.flsw@gmail.com>
2025-02-19 16:26:49 -0600

    edit content home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Department from "./components/layouts/Department";
 import { IconFrame, IconSchool, IconServer, IconTable, IconTrendingUp, IconWorld } from "./components/icons";
-import { companyMail, companyPhone, companyUbication, linkDevelopment, linkMail, linkMap, linkMarketing, linkPhone } from "./ui/links";
+import { companyMail, companyPhone, companyUbication, linkDevelopment, linkMail, linkMap, linkMarketing, linkPhone, textCompany } from "./ui/consts";
 import Quote from "./components/ui/Quote";
 import CardFusion from "./components/ui/CardFusion";
 import ItemList from "./components/ui/ItemList";
@@ -9,14 +9,17 @@ import Button from "./components/ui/Button";
 
 export default function Home() {
   return (
-    <Department>
+    <Department classDescription="hidden md:flex">
       <main className="space-y-12 xl:space-y-14">
-        <h2 className="font-brand text-4xl lg:text-5xl 2xl:text-6xl select-none">Profesionalización Virtual</h2>
+        <div className="flex flex-col gap-4 pb-4">
+          <h2 className="font-brand font-[450] text-4xl md:text-5xl xl:text-6xl 2xl:text-7xl select-none">Profesionalización Virtual</h2>
+          <h2 className="font-brand font-extralight text-pretty text-2xl md:hidden">{textCompany}</h2>
+        </div>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 lg:gap-8 xl:gap-9">
-          <div className="space-y-2.5 lg:space-y-3">
-            <h3 className="font-medium text-lg">Creamos para</h3>
-            <ul>
+          <div className="space-y-3">
+            <h3 className="font-medium text-xl">Creamos para</h3>
+            <ul className="text-primary-300">
               <ItemList isFirst={true} className="leading-tight">
                 Empresas consolidadas
               </ItemList>
@@ -34,13 +37,15 @@ export default function Home() {
               </ItemList>
             </ul>
           </div>
-          <p className="text-pretty text-primary-300">Un diseño atractivo es solo el <span className="font-brand">inicio</span>. La estructura bien pensada <span className="font-brand">potencia</span> la identidad de una marca, la experiencia de usuario <span className="font-brand">eficiente</span> convierte visitantes en <span className="font-brand">clientes</span> y la tecnología escalable permite crecer con <span className="font-brand">autonomía</span>. Un proceso <span className="font-brand">optimizado</span> para resultados impactantes.</p>
+          <div className="h-full flex items-center sm:items-end">
+            <p className="text-sm md:text-base text-pretty text-primary-300">Un diseño atractivo es solo el inicio. La estructura bien pensada potencia la identidad de una marca, la experiencia de usuario eficiente convierte visitantes en clientes y la tecnología escalable permite crecer con autonomía. Un proceso optimizado para resultados impactantes.</p>
+          </div>
         </div>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 lg:gap-8 xl:gap-9">
-          <div className="space-y-2.5 lg:space-y-3">
-            <Button isLink={true} href={linkDevelopment} variant="text" size="lg">Desarrollo</Button>
-            <ul>
+          <div className="space-y-3">
+            <Button isLink={true} href={linkDevelopment} variant="text" size="xl">Desarrollo</Button>
+            <ul className="text-primary-300">
               <ItemList isFirst={true}>
                 Apps, páginas y sitios web
               </ItemList>
@@ -55,9 +60,9 @@ export default function Home() {
               </ItemList>
             </ul>
           </div>
-          <div className="space-y-2.5 lg:space-y-3">
-            <Button isLink={true} href={linkMarketing} variant="text" size="lg">Marketing</Button>
-            <ul>
+          <div className="space-y-3">
+            <Button isLink={true} href={linkMarketing} variant="text" size="xl">Marketing</Button>
+            <ul className="text-primary-300">
               <ItemList isFirst={true}>
                 Branding
               </ItemList>
@@ -79,7 +84,7 @@ export default function Home() {
             <Quote quote="La tecnología no es un mal, ni un fin tampoco; es una herramienta. Es algo que nos permite ayudar, ser más eficientes y productivos; y si la sabemos usar bien, nos irá muy bien."
                   author="Dr. Héctor, CEO de Profesionalización Virtual." link="https://www.facebook.com/share/v/15wJHf8ekQ/" textLink="Entrevista completa" />
           </div>
-          <ul className="order-1 md:order-2">
+          <ul className="order-1 md:order-2 text-primary-300">
             <ItemList isFirst={true}>
               <h4>Diseño Funcional</h4>
               <IconFrame size="18" />
@@ -109,17 +114,17 @@ export default function Home() {
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 xl:gap-9">
           <div>
-            <CardFusion className="font-semibold text-2xl lg:text-3xl xl:text-4xl">Hagamos algo increíble...</CardFusion>
+            <CardFusion className="font-semibold text-3xl lg:text-4xl">Hagamos algo increíble...</CardFusion>
           </div>
           <ul>
             <ItemList isFirst={true}>
-              <Link href={linkMail} className="transition duration-150 group-hover:text-white">{companyMail}</Link>
+              <Link href={linkMail} className="w-full text-center md:text-start transition duration-150 group-hover:text-white">{companyMail}</Link>
             </ItemList>
             <ItemList className="tracking-wide">
-              <Link href={linkPhone} className="transition duration-150 group-hover:text-white">{companyPhone}</Link>
+              <Link href={linkPhone} className="w-full text-center md:text-start transition duration-150 group-hover:text-white">{companyPhone}</Link>
             </ItemList>
             <ItemList className="leading-tight">
-              <Link href={linkMap} className="transition duration-150 group-hover:text-white">{companyUbication}</Link>
+              <Link href={linkMap} className="w-full text-center md:text-start transition duration-150 group-hover:text-white">{companyUbication}</Link>
             </ItemList>
           </ul>
         </div>
