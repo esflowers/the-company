8b1600c2bfa12c33e0a700a541bb10cc2fc2f085
esflowers <estrada.flsw@gmail.com>
2025-02-20 19:21:22 -0600

    add basic content to courses page

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,15 +1,21 @@
 import { IconSchool } from "../components/icons"
 import Department from "../components/layouts/Department"
+import SectionContact from "../components/ui/SectionContact"
+import { textCourses } from "../ui/consts"
 
 export default function Courses() {
   return (
-    <Department mode="edu" title="Cursos" description="Aprende, aplica y crece con formación digital.">
-      <main className="flex flex-col gap-5">
-        <h2 className="font-brand text-2xl md:text-4xl font-medium mb-2">Cursos en línea</h2>
-        <div className="h-96 flex flex-col items-center justify-center gap-1.5">
-            <IconSchool size="40" />
-            <h2 className="font-brand text-center text-lg md:text-xl">Cursos en desarrollo...</h2>
+    <Department mode="edu" title="Cursos" description={textCourses}>
+      <main className="mt-6 md:mt-0 space-y-12 xl:space-y-14">
+        <div className="space-y-4 lg:space-y-6">
+          <h2 className="h2">Cursos en línea</h2>
+          <div className="h-96 flex flex-col items-center justify-center gap-1.5">
+            <IconSchool size="40" stroke="2" />
+            <h2 className="font-medium text-center text-xl">Cursos en desarrollo...</h2>
+          </div>
         </div>
+
+        <SectionContact />
       </main>
     </Department>
   )
