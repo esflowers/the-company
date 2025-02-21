52b2e5228af0b557802588120b1cb865c12c808a
esflowers <estrada.flsw@gmail.com>
2025-02-20 21:14:52 -0600

    add descriptions of pages

--- src/app/ui/consts.ts
+++ src/app/ui/consts.ts
@@ -3,11 +3,11 @@ export const companyPhone = '+529613006743'
 export const companyUbication = 'Cuauhtémoc 188, San Jacinto, 29160 Chiapa de Corzo, Chiapas, México'
 
 export const textCompany = 'Somos una agencia creativa especializada en soluciones digitales.'
-export const textContact = ''
-export const textDevelopment = ''
-export const textMarketing = ''
-export const textCourses = ''
-export const textBlog = ''
+export const textContact = 'Hablemos sobre tu proyecto y cómo podemos ayudarte.'
+export const textDevelopment = 'Creamos productos que conectan, funcionan y evolucionan.'
+export const textMarketing = 'Estrategias que posicionan, atraen y convierten.'
+export const textCourses = 'Aprende, aplica y crece con educación en línea.'
+export const textBlog = 'Tecnología, tendencias y más, en nuestro contenido.'
 
 export const linkCompany = 'https://profesionalizacionvirtual.com/'
 export const linkMail = `mailto:${companyMail}`
