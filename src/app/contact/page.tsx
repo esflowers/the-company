b34ad8fdde8c154a50993709e6abd524098f03bb
esflowers <estrada.flsw@gmail.com>
2025-02-20 21:35:15 -0600

    add description contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,16 +7,15 @@ import { companyMail, companyPhone, companyUbication, linkMail, linkPhone, linkM
 
 export default function Contact() {
     return (
-        <Department classDescription="hidden md:flex" title="Contacto" description={textContact}>
+        <Department title="Contacto" description={textContact}>
             <main className="mt-6 md:mt-0 space-y-4 lg:space-y-6">
                 <div className="h2">Hagamos algo increíble...</div>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 xl:gap-9">
-                    <form action="#" className="space-y-4 lg:space-y-6 md:col-span-2">
+                    <form action="#" className="space-y-6 lg:space-y-8 md:col-span-2">
                         <Input type="text" placeholder="Nombre" />
                         <Input type="text" placeholder="Nombre de la empresa" />
                         <Input type="email" placeholder="Correo electrónico" />
                         <Input type="text" placeholder="¿Qué quieres construir?" />
-                        <br />
                         <Button type="submit" variant="outline" size="xl">Enviar</Button>
                     </form>
                     <div className="flex flex-col sm:flex-col-reverse gap-4">
